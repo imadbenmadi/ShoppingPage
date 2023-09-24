6517a4e5940d1b8b890c281c8c8fe25736ecf195
imadbenmadi <imadbenmadi891@gmail.com>
2023-09-24 20:59:28 +0100

    kk

--- src/Routes/cart.jsx
+++ src/Routes/cart.jsx
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+
 
 export default function Cart() {
-    let cartItems = localStorage.getItem("Cart_Items")
-        ? JSON.parse(localStorage.getItem("Cart_Items"))
-        : [];
-    // const itemsList = cartItems.map((item, index) => (
-    // <div key={index}>
-    //     <p>Item title: {item.title}</p>
-    //     <p>Item Price: {item.price}</p>
-    // </div>
+    const [cartItems, setCartItems] = useState(
+        localStorage.getItem("Cart_Items")
+            ? JSON.parse(localStorage.getItem("Cart_Items"))
+            : []
+    );
+
+    const handleDeleteItem = (productId) => {
+        const updatedCartItems = cartItems.filter(
+            (product) => product.id !== productId
+        );
+
+        // Update the cart state and save it to localStorage
+        setCartItems(updatedCartItems);
+        localStorage.setItem("Cart_Items", JSON.stringify(updatedCartItems));
+    };
+
     const itemsList = (
         <ul className="grid grid-cols-2">
             {cartItems.map((product) => (
@@ -22,14 +30,21 @@ export default function Cart() {
                         alt=""
                         className="max-w-full h-[150px] m-auto"
                     />
-
                     <br />
                     <div className="text-2xl font-bold text-center">
                         {product.price} $
                     </div>
-                    <button className="  bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-full mt-5 m-auto w-fit">
-                        Check Out
-                    </button>
+                    <div className="flex ">
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-full mt-5 m-auto w-fit">
+                            Check Out
+                        </button>
+                        <button
+                            className="bg-red-500 hover:bg-red-700 text-white font-bold py-3 px-6 rounded-full mt-5 m-auto w-fit"
+                            onClick={() => handleDeleteItem(product.id)}
+                        >
+                            Delete
+                        </button>
+                    </div>
                 </li>
             ))}
         </ul>
@@ -39,13 +54,9 @@ export default function Cart() {
         <div>
             <h1 className="text-start text-3xl font-bold">Your Cart</h1>
             <div>
-                <div>
+                <div className="mt-10">
                     <span>Your Wallet :</span>
                     <i className="Money">1000$</i>
-                </div>
-                <div>
-                    <img src="" alt="" />
-                    <img src="" alt="" />
                 </div>
             </div>
             {itemsList}
